fda8079342f7d7b8c7e49a50ad881e599232e60f
zerotohero200222 <aebharath17@gmail.com>
2025-12-05 19:41:39 +0530

    Update variables.tf

--- modules/cloud_run_service/variables.tf
+++ modules/cloud_run_service/variables.tf
@@ -1,15 +1,46 @@
-variable "project_id" { type = string }
-variable "region"     { type = string }
-variable "name"       { type = string }
-variable "image"      { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
 
 variable "allow_unauthenticated" {
   type    = bool
   default = false
 }
 
-variable "cpu"         { type = string, default = null }
-variable "memory"      { type = string, default = null }
-variable "min_instances" { type = number, default = null }
-variable "max_instances" { type = number, default = null }
-variable "env"          { type = map(string), default = {} }
+variable "cpu" {
+  type    = string
+  default = null
+}
+
+variable "memory" {
+  type    = string
+  default = null
+}
+
+variable "min_instances" {
+  type    = number
+  default = null
+}
+
+variable "max_instances" {
+  type    = number
+  default = null
+}
+
+variable "env" {
+  type    = map(string)
+  default = {}
+}
+
