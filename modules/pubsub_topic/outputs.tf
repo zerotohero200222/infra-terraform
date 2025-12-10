fcca04f1c69258d3117c88282c90cd1c3c91209d
zerotohero200222 <aebharath17@gmail.com>
2025-12-10 08:32:07 +0530

    Update outputs.tf for Pub/Sub subscription details

--- modules/pubsub_topic/outputs.tf
+++ modules/pubsub_topic/outputs.tf
@@ -1,3 +1,9 @@
 output "name" {
-  value = google_pubsub_topic.topic.name
+  description = "Pub/Sub subscription name"
+  value       = google_pubsub_subscription.subscription.name
+}
+
+output "id" {
+  description = "Pub/Sub subscription ID"
+  value       = google_pubsub_subscription.subscription.id
 }
