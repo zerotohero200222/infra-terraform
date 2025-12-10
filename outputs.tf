6988a466accf87f3cac99344d28f42b7daa11e37
zerotohero200222 <aebharath17@gmail.com>
2025-12-10 08:30:47 +0530

    Enhance outputs.tf with descriptions and new outputs
    Added descriptions for outputs related to Pub/Sub topics and subscriptions, and created new outputs for subscriptions and service account.

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "service_urls" {
-  description = "Cloud Run deployed service URLs"
+  description = "Map of Cloud Run service URLs"
   value = {
     for name, mod in module.cloud_run_services :
     name => mod.url
@@ -7,9 +7,26 @@ output "service_urls" {
 }
 
 output "exchanges_topic" {
-  value = module.exchanges_topic.name
+  description = "Exchanges Pub/Sub topic name"
+  value       = module.exchanges_topic.name
 }
 
 output "tracking_topic" {
-  value = module.tracking_topic.name
+  description = "Tracking Pub/Sub topic name"
+  value       = module.tracking_topic.name
+}
+
+output "exchanges_subscription" {
+  description = "Exchanges Pub/Sub subscription name"
+  value       = module.exchanges_subscription.name
+}
+
+output "tracking_subscription" {
+  description = "Tracking Pub/Sub subscription name"
+  value       = module.tracking_subscription.name
+}
+
+output "service_account" {
+  description = "Service account used by Cloud Run and Pub/Sub"
+  value       = var.service_account_email
 }
