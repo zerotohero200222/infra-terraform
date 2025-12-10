06d063b49becb21bcb676450505b18c329638d09
zerotohero200222 <aebharath17@gmail.com>
2025-12-10 08:29:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,23 +35,94 @@ module "tracking_topic" {
 }
 
 ############################################
+# Pub/Sub Subscriptions
+############################################
+
+module "exchanges_subscription" {
+  source = "./modules/pubsub_subscription"
+
+  project_id        = var.project_id
+  subscription_name = var.exchanges_subscription_name
+  topic_name        = module.exchanges_topic.name
+  service_account   = var.service_account_email
+
+  depends_on = [module.exchanges_topic]
+}
+
+module "tracking_subscription" {
+  source = "./modules/pubsub_subscription"
+
+  project_id        = var.project_id
+  subscription_name = var.tracking_subscription_name
+  topic_name        = module.tracking_topic.name
+  service_account   = var.service_account_email
+
+  depends_on = [module.tracking_topic]
+}
+
+############################################
+# Grant Pub/Sub IAM permissions
+############################################
+
+# Grant publisher role on exchanges topic
+resource "google_pubsub_topic_iam_member" "exchanges_publisher" {
+  project = var.project_id
+  topic   = module.exchanges_topic.name
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${var.service_account_email}"
+
+  depends_on = [module.exchanges_topic]
+}
+
+# Grant publisher role on tracking topic
+resource "google_pubsub_topic_iam_member" "tracking_publisher" {
+  project = var.project_id
+  topic   = module.tracking_topic.name
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${var.service_account_email}"
+
+  depends_on = [module.tracking_topic]
+}
+
+# Grant subscriber role on exchanges subscription
+resource "google_pubsub_subscription_iam_member" "exchanges_subscriber" {
+  project      = var.project_id
+  subscription = module.exchanges_subscription.name
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${var.service_account_email}"
+
+  depends_on = [module.exchanges_subscription]
+}
+
+# Grant subscriber role on tracking subscription
+resource "google_pubsub_subscription_iam_member" "tracking_subscriber" {
+  project      = var.project_id
+  subscription = module.tracking_subscription.name
+  role         = "roles/pubsub.subscriber"
+  member       = "serviceAccount:${var.service_account_email}"
+
+  depends_on = [module.tracking_subscription]
+}
+
+############################################
 # Cloud Run Services
 ############################################
 
 module "cloud_run_services" {
-  for_each  = var.services
-  source    = "./modules/cloud_run_service"
+  for_each = var.services
+  source   = "./modules/cloud_run_service"
 
   project_id = var.project_id
   region     = var.region
   name       = each.key
   image      = each.value.image
 
+  service_account       = var.service_account_email
   allow_unauthenticated = try(each.value.allow_unauthenticated, false)
 
-  cpu            = try(each.value.cpu, null)
-  memory         = try(each.value.memory, null)
-  min_instances  = try(each.value.min_instances, null)
-  max_instances  = try(each.value.max_instances, null)
-  env            = try(each.value.env, null)
+  cpu           = try(each.value.cpu, null)
+  memory        = try(each.value.memory, null)
+  min_instances = try(each.value.min_instances, null)
+  max_instances = try(each.value.max_instances, null)
+  env           = try(each.value.env, {})
 }
