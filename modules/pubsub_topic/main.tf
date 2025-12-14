d00b43a64fa957108b4eadc7934e6150f00f54f4
zerotohero200222 <aebharath17@gmail.com>
2025-12-14 17:16:11 +0530

    Modify Pub/Sub subscription parameters
    Updated subscription ack_deadline_seconds and message_retention_duration.

--- modules/pubsub_topic/main.tf
+++ modules/pubsub_topic/main.tf
@@ -1,21 +1,18 @@
 resource "google_pubsub_topic" "topic" {
-  name      = var.topic_name
-  project   = var.project_id
-  labels    = var.labels
+  name    = var.topic_name
+  project = var.project_id
+  labels  = var.labels
 }
 
 resource "google_pubsub_subscription" "subscription" {
-  name                         = var.subscription_name
-  project                      = var.project_id
-  topic                        = google_pubsub_topic.topic.name
-  ack_deadline_seconds         = var.ack_deadline_seconds
-  message_retention_duration   = var.message_retention_duration
+  name                       = var.subscription_name
+  project                    = var.project_id
+  topic                      = google_pubsub_topic.topic.name
+  ack_deadline_seconds       = 10
+  message_retention_duration = "604800s"
 
   expiration_policy {
     ttl = ""
   }
-
-  labels = {
-    managed-by = "terraform"
-  }
 }
+
