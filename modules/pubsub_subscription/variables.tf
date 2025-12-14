eec09bd70086fbbdab744983ab0f408839a5be2b
zerotohero200222 <aebharath17@gmail.com>
2025-12-14 16:47:43 +0530

    Update variables.tf

--- modules/pubsub_subscription/variables.tf
+++ modules/pubsub_subscription/variables.tf
@@ -1,26 +1,31 @@
 variable "project_id" {
-  description = "GCP project ID"
-  type        = string
+  type = string
 }
 
 variable "subscription_name" {
-  description = "Pub/Sub subscription name"
-  type        = string
+  type = string
 }
 
 variable "topic_name" {
-  description = "Pub/Sub topic name to subscribe to"
-  type        = string
+  type = string
 }
 
 variable "ack_deadline_seconds" {
-  description = "Acknowledgment deadline in seconds"
-  type        = number
-  default     = 10
+  type    = number
+  default = 10
 }
 
 variable "message_retention_duration" {
-  description = "Message retention duration"
+  type    = string
+  default = "604800s"
+}
+
+variable "labels" {
+  type    = map(string)
+  default = {}
+}
+
+variable "service_account" {
+  description = "Service account email that will pull from the subscription"
   type        = string
-  default     = "604800s"
 }
