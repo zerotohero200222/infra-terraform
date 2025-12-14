8a086acc2df033abd688f0b0750d4c7ddc84e633
zerotohero200222 <aebharath17@gmail.com>
2025-12-14 17:23:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,20 @@
-output "service_urls" {
-  description = "Map of Cloud Run service URLs"
-  value = {
-    for name, mod in module.cloud_run_services :
-    name => mod.url
-  }
-}
-
 output "exchanges_topic" {
-  description = "Exchanges Pub/Sub topic name"
+  description = "Exchanges Pub/Sub topic"
   value       = module.exchanges_topic.name
 }
 
 output "tracking_topic" {
-  description = "Tracking Pub/Sub topic name"
+  description = "Tracking Pub/Sub topic"
   value       = module.tracking_topic.name
 }
 
 output "exchanges_subscription" {
-  description = "Exchanges Pub/Sub subscription name"
+  description = "Exchanges subscription"
   value       = module.exchanges_subscription.name
 }
 
 output "tracking_subscription" {
-  description = "Tracking Pub/Sub subscription name"
+  description = "Tracking subscription"
   value       = module.tracking_subscription.name
 }
 
-output "service_account" {
-  description = "Service account used by Cloud Run and Pub/Sub"
-  value       = var.service_account_email
-}
