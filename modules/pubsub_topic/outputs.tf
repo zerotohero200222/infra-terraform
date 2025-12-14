dedf6c1aa03c5aa286fdc641a913def112294836
zerotohero200222 <aebharath17@gmail.com>
2025-12-14 17:22:51 +0530

    Update outputs.tf

--- modules/pubsub_topic/outputs.tf
+++ modules/pubsub_topic/outputs.tf
@@ -1,3 +1,8 @@
+output "name" {
+  description = "Pub/Sub Topic name"
+  value       = google_pubsub_topic.topic.name
+}
+
 output "topic_name" {
   description = "Pub/Sub Topic name"
   value       = google_pubsub_topic.topic.name
@@ -7,4 +12,5 @@ output "subscription_name" {
   description = "Pub/Sub Subscription name"
   value       = google_pubsub_subscription.subscription.name
 }
+
 
