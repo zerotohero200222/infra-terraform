baeee02a58e604893aa03374e7d520c57fa32889
zerotohero200222 <aebharath17@gmail.com>
2025-12-17 07:34:00 +0530

    change the provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  # Terraform Cloud Backend
+  cloud {
+    organization = "your-org-name"  # Replace with your org name
+
+    workspaces {
+      name = "gcp-infra-dev"
+    }
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -12,4 +21,6 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
+  
+  # Credentials are provided via GOOGLE_CREDENTIALS env var in Terraform Cloud
 }
