972d69d76729cae7007f7278ae71e37841c3b31d
zerotohero200222 <aebharath17@gmail.com>
2026-01-05 15:52:03 +0530

    feature-0.2

--- modules/pubsub_subscription/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "Pub/Sub subscription name"
-  value       = google_pubsub_subscription.subscription.name
-}
-
-output "id" {
-  description = "Pub/Sub subscription ID"
-  value       = google_pubsub_subscription.subscription.id
-}
